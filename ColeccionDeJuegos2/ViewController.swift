2b0d4289c1f1d430870bcb29d3a7063c2b881a6f
angelo3033 <angel.samanez@tecsup.edu.pe>
2021-05-18 13:00:12 -0500

    Initial Commit

---
+++ ColeccionDeJuegos2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos2
+//
+//  Created by Mac 11 on 5/18/21.
+//  Copyright © 2021 ColeccionDeJuegos2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
