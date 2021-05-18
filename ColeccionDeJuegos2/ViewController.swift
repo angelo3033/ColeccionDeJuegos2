73adaa90f7d4e5a6056c6da962e8874fb43b45c1
angelo3033 <angel.samanez@tecsup.edu.pe>
2021-05-18 17:49:13 -0500

    segunda parte del avance

--- ColeccionDeJuegos2/ViewController.swift
+++ ColeccionDeJuegos2/ViewController.swift
@@ -21,7 +21,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let juego = juegos[indexPath.row]
+        performSegue(withIdentifier: "juegoSegue", sender: juego)
+    }
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -46,6 +49,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             
         }
     
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let siguienteVC = segue.destination as! JuegosViewController
+        siguienteVC.juego = sender as? Juego
     }
 
 
